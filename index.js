03e3eb24b8f8dc13e22f624e04f066460e517c1c
arifislamarif344 <arifislamarif344@gmail.com>
2022-11-08 12:00:23 +0600

    read steam

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.write("Hello Programmers!");
-    res.write("How are you doing");
+    res.write("<html><head><title>Form</title></head>");
+    res.write(
+      '<body><form method="post" action="/process"><input name="message" /></form></body>'
+    );
     res.end();
-  } else if (req.url === "/about") {
-    res.write("Abour us page");
-    res.end();
+  } else if (req.url === "/process" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      console.log("stream finished");
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      res.write("Thank you for submitting");
+      res.end();
+    });
   } else {
-    res.write("Not Found");
+    res.write("Not found");
     res.end();
   }
 });
-server.listen(3000);
 
-console.log("Listening on port 3000");
+server.listen(3000);
+console.log("Listening port on 3000");
