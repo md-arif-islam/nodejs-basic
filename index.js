897180ead6c8bc4a6278ffe3966e6b08f12d04e4
arifislamarif344 <arifislamarif344@gmail.com>
2022-11-09 09:36:23 +0600

    Pipe

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ const fs = require("fs");
 const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
 const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
-ourReadStream.on("data", (chunk) => {
-  ourWriteStream.write(chunk);
-});
+ourReadStream.pipe(ourWriteStream);
