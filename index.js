dad50d6c90f49e6da8b6934a9d3f8c95823fe990
arifislamarif344 <arifislamarif344@gmail.com>
2022-11-09 09:36:08 +0600

    write stream

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("<html><head><title>Form</title></head>");
-    res.write(
-      '<body><form method="post" action="/process"><input name="message" /></form></body>'
-    );
-    res.end();
-  } else if (req.url === "/process" && req.method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      console.log("stream finished");
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      res.write("Thank you for submitting");
-      res.end();
-    });
-  } else {
-    res.write("Not found");
-    res.end();
-  }
+const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+
+ourReadStream.on("data", (chunk) => {
+  ourWriteStream.write(chunk);
 });
-
-server.listen(3000);
-console.log("Listening port on 3000");
